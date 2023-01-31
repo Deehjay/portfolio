a621d5779413b7531d78343862e0d93584dd3c89
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-31 12:36:17 +0000

    initial commit - project setup

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Work</a>
+    <a href="#">Contact</a>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  height: 5vh;
+  border-bottom: 1px solid black;
+}
+</style>
