087b181ea1eababeae61bcd4eb0e502f8c86e99c
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-01 15:53:30 +0000

    Add vue-router to app, change css file import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./index.css";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
