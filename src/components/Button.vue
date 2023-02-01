122c49555b2fc082fb7400c8c4ff90f97e2c9fe2
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-01 15:54:59 +0000

    Create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <button class="text-white rounded bg-gray-600 p-2 uppercase">
+    {{ text }}
+  </button>
+</template>
