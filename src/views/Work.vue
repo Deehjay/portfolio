9dee7d5710b3044df01e459c057b246610139ee3
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-06 15:48:53 +0000

    Create work view

---
+++ src/views/Work.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { projects } from "../projects.js";
+import ProjectCard from "../components/ProjectCard.vue";
+</script>
+
+<template>
+  <main>
+    <ProjectCard
+      v-for="project in projects"
+      :projectName="project.projectName"
+      :slug="project.slug"
+      :cardImage="project.cardImage" />
+  </main>
+</template>
