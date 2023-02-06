b420e655dd9411ca33b65d58dc1ba37ef8e275b0
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-06 15:46:56 +0000

    Create additional paths

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Work from "./views/Work.vue";
+import SingleProject from "./components/SingleProject.vue";
+import Contact from "./views/Contact.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +17,10 @@ const router = createRouter({
       component: Home,
     },
     { path: "/about", name: "About", component: About },
+    { path: "/work", name: "Work", component: Work },
+
+    { path: "/work/:projectName", name: "Project", component: SingleProject },
+    { path: "/contact", name: "Contact", component: Contact },
   ],
 });
 
