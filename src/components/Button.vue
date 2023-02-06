5f32b514fca5e4354afb4381b3605e9c535d5678
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-06 15:46:19 +0000

    Change button to be more dynamic for internal/external routes

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,23 @@
 <script setup>
 defineProps({
   text: String,
+  internal: Boolean,
+  link: String,
 });
 </script>
 
 <template>
-  <button class="text-white rounded bg-gray-600 p-2 uppercase">
-    {{ text }}
-  </button>
+  <router-link
+    v-if="internal"
+    :to="link"
+    class="text-white rounded bg-gray-600 p-2 uppercase">
+    {{ text }}</router-link
+  >
+  <a
+    v-else
+    :href="link"
+    class="text-white rounded bg-gray-600 p-2 w-20 text-center uppercase"
+    target="_blank"
+    >{{ text }}</a
+  >
 </template>
