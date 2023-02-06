348b800878c354c9bf976502f1cb55848a3e6381
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-06 15:48:42 +0000

    Create contact view

---
+++ src/views/Contact.vue
@@ -0,0 +1,53 @@
+<script>
+import { postContactEmail } from "../api.js";
+
+export default {
+  // data() {
+  //   return {
+  //     name: "",
+  //     email: "",
+  //     phone: "",
+  //     message: "",
+  //   };
+  // },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      const postOptions = {
+        name: this.$refs.name.value,
+        email: this.$refs.email.value,
+        phone: this.$refs.phone.value,
+        message: this.$refs.message.value,
+      };
+
+      //
+
+      postContactEmail(postOptions)
+        .then(() => {
+          console.log("Email sent");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      // this.$refs.contactForm.reset();
+    },
+  },
+};
+</script>
+
+<template>
+  <form ref="contactForm" action="">
+    <input type="text" ref="name" />
+    <input type="email" ref="email" />
+    <input type="text" ref="phone" />
+    <input type="text" ref="message" />
+    <button @click="handleSubmit">Submit</button>
+  </form>
+</template>
+
+<style>
+button {
+  color: black;
+}
+</style>
