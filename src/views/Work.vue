c2cc1d2844f51b0b081f83b6e02072ac3646ccba
dj <41585360+Deehjay@users.noreply.github.com>
2023-02-08 17:43:40 +0000

    Work page styling

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -5,10 +5,29 @@ import ProjectCard from "../components/ProjectCard.vue";
 
 <template>
   <main>
-    <ProjectCard
-      v-for="project in projects"
-      :projectName="project.projectName"
-      :slug="project.slug"
-      :cardImage="project.cardImage" />
+    <div class="projects-container flex justify-center items-center">
+      <div
+        class="projects-inner w-1/2 flex items-center justify-center flex-wrap">
+        <ProjectCard
+          v-for="project in projects"
+          :projectName="project.projectName"
+          :slug="project.slug"
+          :cardImage="project.cardImage"
+          :cardHeading="project.cardHeading" />
+      </div>
+    </div>
   </main>
 </template>
+
+<style>
+.projects-container {
+  height: 91vh;
+}
+
+.projects-inner {
+  max-height: 100%;
+  opacity: 0;
+  animation: fadeIn 2s;
+  animation-fill-mode: forwards;
+}
+</style>
