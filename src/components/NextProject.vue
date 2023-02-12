1cc9d8d8ac5d861a8a50d08ada7df9898fd1cf52
Deehjay <danielcjamesdj@gmail.com>
2023-02-12 19:52:31 +0000

    Create next project component for single projects

---
+++ src/components/NextProject.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: { currProject: String },
+  computed: {
+    nextProject() {
+      const projects = [
+        "know-it-all",
+        "portfolio",
+        "house-of-games-api",
+        "house-of-games",
+      ];
+      const currentIndex = projects.indexOf(this.currProject);
+      return projects[(currentIndex + 1) % projects.length];
+    },
+  },
+};
+</script>
+
+<template>
+  <router-link
+    class="text-white bg-gray-600 p-4 uppercase text-center drop-shadow-lg"
+    :to="`/work/${nextProject}`"
+    >Next Project</router-link
+  >
+</template>
