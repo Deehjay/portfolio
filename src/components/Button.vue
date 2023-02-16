55dea8d85e1e6da6b5d841a08b4df6694f1751fe
Deehjay <danielcjamesdj@gmail.com>
2023-02-16 14:25:55 +0000

    Button styling for different devices

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -21,13 +21,13 @@ export default {
   <router-link
     v-if="internal"
     :to="link"
-    class="text-white bg-gray-600 p-4 uppercase text-center drop-shadow-lg text-xl rounded laptoplg:text-lg laptoplg:p-2">
+    class="text-white bg-gray-600 p-4 uppercase text-center drop-shadow-lg text-xl rounded laptoplg:text-lg laptoplg:p-2 phone:text-base">
     {{ text }}</router-link
   >
   <a
     v-else
     :href="link"
-    class="text-white bg-gray-600 p-3 text-center uppercase drop-shadow-lg rounded"
+    class="text-white bg-gray-600 p-3 text-center uppercase drop-shadow-lg rounded phone:text-base"
     target="_blank"
     >{{ text }}</a
   >
@@ -40,5 +40,6 @@ a {
 
 a:hover {
   background-color: #68cc8c;
+  color: #333d4d;
 }
 </style>
